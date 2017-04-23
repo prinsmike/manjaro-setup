c5f3a6ffaa48f657735624766190239a4c8534ed
prinsmike <mike.prinsloo@gmail.com>
2017-04-23 17:26:44 +0200

    updated printers

--- printers.sh
+++ printers.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-sudo pacman --noconfirm -S gutenprint foomatic-db-gutenprint-ppds sane xsane
+sudo pacman --noconfirm -Sy manjaro-printer gutenprint foomatic-db-gutenprint-ppds sane xsane
+
+sudo systemctl enable org.cups.cupsd.service
+sudo systemctl start ort.cups.cupsd.service
