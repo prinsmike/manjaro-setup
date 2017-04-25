9942259b3a64e6c1f02f0271f4343038f87a63a3
prinsmike <mike.prinsloo@gmail.com>
2017-04-25 12:41:12 +0200

    Changed YCM installation to AUR.

--- vim.sh
+++ vim.sh
@@ -1,30 +1,17 @@
 #!/bin/sh
 
-if [[ "${PWD}" != "/tmp/manjaro-setup" ]]; then
-	echo "This script should be run from /tmp/manjaro-setup."
-	exit 1
-fi
-
 mkdir -p ~/.vim/bundle ~/.vim/colors
 
-sudo pacman --noconfirm -S vim clang ctags
+sudo pacman --noconfirm -S vim clang ctags yaourt
 
 sudo /bin/sh -c 'echo "export EDITOR=vim" >> /etc/profile.d/env.sh'
 
 cp ./.vimrc ~/.vimrc
 cp ./molokai.vim ~/.vim/colors
 
-# Build YouCompleteMe
-git clone --depth 1 https://github.com/Valloric/YouCompleteMe.git \
-	~/.vim/bundle/YouCompleteMe/ && \
-	echo "Changing to YouCompleteMe directory." && \
-	cd ~/.vim/bundle/YouCompleteMe && \
-	pwd && \
-	git submodule update --init --recursive && \
-	~/.vim/bundle/YouCompleteMe/install.py --clang-completer && \
-	echo "Changing back to /tmp/manjaro-setup." && \
-	cd /tmp/manjaro-setup && \
-	pwd
+# Install YouCompleteMe
+gpg --recv-keys C52048C0C0748FEE227D47A2702353E0F7E48EDB
+yaourt -S --noconfirm vim-youcompleteme-git
 
 # Install Vundle and plugins
 git clone https://github.com/gmarik/vundle ~/.vim/bundle/vundle && \
