e6124c59cf735ee153cc108f20eac0d0833c4f37
prinsmike <mike.prinsloo@gmail.com>
2017-06-29 17:49:41 +0200

    Added xsane-gimp package, and fixed invalid delay in battmon.sh

--- battmon.sh
+++ battmon.sh
@@ -10,7 +10,7 @@ BATTTIME=`echo $BATTINFO | cut -f 5 -d " "`
 
 
 if [[ `echo $BATTINFO | grep Discharging` && $BATTPERC < 22 ]]; then
-	/usr/bin/notify-send -u critical -t 9999999999 "Battery discharging: $BATTPERC%\nRemaining: $BATTTIME"
+	/usr/bin/notify-send -u critical -t 99999999 "Battery discharging: $BATTPERC%\nRemaining: $BATTTIME"
 elif [[ `echo $BATTINFO | grep Charging` && $BATTPERC > 78 ]]; then
-	/usr/bin/notify-send -u critical -t 9999999999 "Battery charging: $BATTPERC%\nRemaining: $BATTTIME"
+	/usr/bin/notify-send -u critical -t 99999999 "Battery charging: $BATTPERC%\nRemaining: $BATTTIME"
 fi
--- printers.sh
+++ printers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo pacman --noconfirm -Sy manjaro-printer gutenprint foomatic-db-gutenprint-ppds sane xsane
+sudo pacman --noconfirm -Sy manjaro-printer gutenprint foomatic-db-gutenprint-ppds sane xsane xsane-gimp
 
 sudo systemctl enable org.cups.cupsd.service
 sudo systemctl start ort.cups.cupsd.service
