034daa6c889f70b3a63181a4d0c94c57a5be7b39
prinsmike <mike.prinsloo@gmail.com>
2017-07-21 20:29:31 +0200

    Reduced notification delay.

--- battmon.sh
+++ battmon.sh
@@ -8,9 +8,12 @@ BATTPERC=`echo $BATTINFO | cut -f 4 -d " "`
 BATTPERC=${BATTPERC%\%,}
 BATTTIME=`echo $BATTINFO | cut -f 5 -d " "`
 
+DISCHARGING=$(printf "Battery discharging: %s%%\nRemaining: %s" "$BATTPERC" "$BATTTIME")
+CHARGING=$(printf "Battery charging: %s%%\nRemaining: %s" "$BATTPERC" "$BATTTIME")
+
 
 if [[ `echo $BATTINFO | grep Discharging` && $BATTPERC < 22 ]]; then
-	/usr/bin/notify-send -u critical -t 1200000 "Battery discharging: $BATTPERC%\nRemaining: $BATTTIME"
+	/usr/bin/notify-send -u critical -t 600000 "$DISCHARGING"
 elif [[ `echo $BATTINFO | grep Charging` && $BATTPERC > 78 ]]; then
-	/usr/bin/notify-send -u critical -t 1200000 "Battery charging: $BATTPERC%\nRemaining: $BATTTIME"
+	/usr/bin/notify-send -u critical -t 600000 "$CHARGING"
 fi
