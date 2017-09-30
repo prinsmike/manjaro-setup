23df58ced9deb2487990198bb830e615286164df
Michael <mike.prinsloo@gmail.com>
2017-09-30 19:49:58 +0200

    Fixing samba.

--- networking.sh
+++ networking.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 sudo pacman -S samba
+
+sudo wget "https://git.samba.org/samba.git/?p=samba.git;a=blob_plain;f=examples/smb.conf.default;hb=HEAD" -O /etc/samba/smb.conf
+
+sudo systemctl enable smbd.service
+sudo systemctl start smbd.service
