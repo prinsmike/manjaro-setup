3763adba72c837b64f8cf3ab31760e5936b05f23
Michael <mike.prinsloo@gmail.com>
2017-09-30 20:33:40 +0200

    Added youtube-dl.

--- util.sh
+++ util.sh
@@ -2,3 +2,7 @@
 
 # iostat
 sudo pacman -S sysstat htop lsof iotop zip unzip bash-completion thunar
+
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+
+sudo chmod a+rx /usr/local/bin/youtube-dl
