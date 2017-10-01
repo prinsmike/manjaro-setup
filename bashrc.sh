f12828ccdedcb72467877af2a09055f5e8533989
Michael <mike.prinsloo@gmail.com>
2017-10-01 15:11:52 +0200

    Fixed typo in printers.sh.

--- bashrc.sh
+++ bashrc.sh
@@ -16,3 +16,4 @@ echo '# User' >> ~/.bashrc
 echo 'export GOPATH=~/go' >> ~/.bashrc
 echo 'export PATH=$PATH:~/bin:$GOPATH/bin:~/.cargo/bin' >> ~/.bashrc
 echo 'export PATH="$PATH:$(ruby -e '\''print Gem.user_dir'\'')/bin"' >> ~/.bashrc
+
--- printers.sh
+++ printers.sh
@@ -3,4 +3,4 @@
 sudo pacman --noconfirm -Sy manjaro-printer gutenprint foomatic-db-gutenprint-ppds sane xsane xsane-gimp
 
 sudo systemctl enable org.cups.cupsd.service
-sudo systemctl start ort.cups.cupsd.service
+sudo systemctl start org.cups.cupsd.service
