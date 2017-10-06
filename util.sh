690d0120f3e57a67ac84847dbb55e31c3475ff05
Michael <mike.prinsloo@gmail.com>
2017-10-06 08:31:24 +0200

    Added google cloud sdk.

--- util.sh
+++ util.sh
@@ -6,3 +6,5 @@ sudo pacman -S sysstat htop lsof iotop zip unzip bash-completion thunar
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 
 sudo chmod a+rx /usr/local/bin/youtube-dl
+
+yaourt -S google-cloud-sdk
