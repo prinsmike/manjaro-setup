2ed6b6f253fee6443c82392471eaaed49af38d48
Michael Prinsloo <mike.prinsloo@gmail.com>
2017-10-08 16:02:48 +0200

    Removed ruby from bashrc.

--- bashrc.sh
+++ bashrc.sh
@@ -17,5 +17,4 @@ echo >> ~/.bashrc
 echo '# User' >> ~/.bashrc
 echo 'export GOPATH=~/go' >> ~/.bashrc
 echo 'export PATH=$PATH:~/bin:$GOPATH/bin:~/.cargo/bin' >> ~/.bashrc
-echo 'export PATH="$PATH:$(ruby -e '\''print Gem.user_dir'\'')/bin"' >> ~/.bashrc
 
