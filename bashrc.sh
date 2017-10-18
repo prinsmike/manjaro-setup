de566fe4059dcbea75b4787ccd254321c566725b
Michael Prinsloo <mike.prinsloo@gmail.com>
2017-10-18 15:29:01 +0200

    Added user info to docker aliases.

--- bashrc.sh
+++ bashrc.sh
@@ -22,14 +22,14 @@ echo 'alias keys="etcd-env keys"' >> ~/.bashrc
 echo 'alias all="etcd-env all"' >> ~/.bashrc
 
 echo >> ~/.bashrc
-echo 'alias goalpine="docker run --rm -v \$GOPATH:\$HOME/go -w \$(pwd) -e GOPATH=\$GOPATH golang:alpine go"'
-echo 'alias node="docker run --rm -v \$(pwd):/app prinsmike/typescript node"'
-echo 'alias npm="docker run --rm -v \$(pwd):/app prinsmike/typescript npm"'
-echo 'alias tsc="docker run --rm -v \$(pwd):/app prinsmike/typescript tsc"'
-echo 'alias gulp="docker run --rm -v \$(pwd):/app prinsmike/typescript gulp"'
-echo 'alias webpack="docker run --rm -v \$(pwd):/app prinsmike/typescript webpack"'
-echo 'alias 'create-react-app="docker run --rm -v \$(pwd):/app prinsmike/typescript create-react-app"'
-echo 'alias sass="docker run --rm -v \$(pwd):\$(pwd) -w \$(pwd) jbergknoff/sass"'
+echo 'alias goalpine="docker run --rm -u=\$UID:\$(id -g \$USER) -v \$GOPATH:\$HOME/go -w \$(pwd) -e GOPATH=\$GOPATH golang:alpine go"'
+echo 'alias node="docker run --rm -u=\$UID:\$(id -g \$USER) -v \$(pwd):/app prinsmike/typescript node"'
+echo 'alias npm="docker run --rm -u=\$UID:\$(id -g \$USER) -v \$(pwd):/app prinsmike/typescript npm"'
+echo 'alias tsc="docker run --rm -u=\$UID:\$(id -g \$USER) -v \$(pwd):/app prinsmike/typescript tsc"'
+echo 'alias gulp="docker run --rm -u=\$UID:\$(id -g \$USER) -v \$(pwd):/app prinsmike/typescript gulp"'
+echo 'alias webpack="docker run --rm -u=\$UID:\$(id -g \$USER) -v \$(pwd):/app prinsmike/typescript webpack"'
+echo 'alias 'create-react-app="docker run --rm -u=\$UID:\$(id -g \$USER) -v \$(pwd):/app prinsmike/typescript create-react-app"'
+echo 'alias sass="docker run --rm -u=\$UID:\$(id -g \$USER) -v \$(pwd):\$(pwd) -w \$(pwd) jbergknoff/sass"'
 
 echo >> ~/.bashrc
 echo '# User' >> ~/.bashrc
