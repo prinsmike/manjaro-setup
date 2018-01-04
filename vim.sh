b905aec68bd4f4da50f8419a89f8a6f8c85e5662
Michael Prinsloo <mike.prinsloo@gmail.com>
2018-01-04 09:31:16 +0200

    Removed YouCompleteMe and added neovim.

--- vim.sh
+++ vim.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# vim
+
 mkdir -p ~/.vim/bundle ~/.vim/colors
 mkdir -p ~/.vim/tmp
 
@@ -10,14 +12,10 @@ sudo /bin/sh -c 'echo "export EDITOR=vim" >> /etc/profile.d/env.sh'
 cp ./.vimrc ~/.vimrc
 cp ./molokai.vim ~/.vim/colors
 
-# Install YouCompleteMe
-gpg --recv-keys C52048C0C0748FEE227D47A2702353E0F7E48EDB
-yaourt -S --noconfirm vim-youcompleteme-git
-
 # Install Vundle and plugins
 git clone https://github.com/gmarik/vundle ~/.vim/bundle/vundle && \
 	vim +PluginInstall +qall!
 
-cd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer
-cd -
+# neovim
+
+sudo pacman --noconfirm -S neovim
