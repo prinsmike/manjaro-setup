bec37c603641824365e4f20b1d3f697621bf1f7b
prinsmike <mike.prinsloo@gmail.com>
2018-12-13 12:58:54 +0200

    yd

--- util.sh
+++ util.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # iostat
-sudo pacman -S sysstat htop lsof iotop zip unzip bash-completion pwgen jq
+sudo pacman --noconfirm -S sysstat htop lsof iotop zip unzip bash-completion pwgen jq
 
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 
 sudo chmod a+rx /usr/local/bin/youtube-dl
 
-yaourt --noconfirm -S google-cloud-sdk
+sudo ln /usr/local/bin/youtube-dl /usr/local/bin/yd
+
